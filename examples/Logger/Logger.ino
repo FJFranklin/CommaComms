f2f37731febe02c39fbba971f2104069038af461
Francis James Franklin <fjf@alinameridon.com>
2022-03-05 20:08:45 +0000

    Add missing file Timer.hh; split out SD into separate class/file.

--- examples/Logger/Logger.ino
+++ examples/Logger/Logger.ino
@@ -1,17 +1,18 @@
 #include <EEPROM.h>
 
-#include <SdFat.h>
-#include <sdios.h>
-
 #include "MultiShell.hh"
+#include "Timer.hh"
+#include "LoggerSD.hh"
 
+/* Define globally
+ */
 ShellCommand sc_hello("hello",    "hello",                         "Say hi :-)");
 ShellCommand sc_lnext("log-next", "log-next [--reset]",            "Next filename to be used when logging.");
 ShellCommand sc_lauto("log-auto", "log-auto [--enable|--disable]", "Whether to start logging automatically on power-up.");
 ShellCommand sc_sdinf("sd-info",  "sd-info",                       "Give details of SD card.");
 ShellCommand sc_lsdir("ls",       "ls",                            "List files on the device.");
-
-#include "Timer.hh"
+ShellCommand sc_demoj("demo",     "demo [--enable|--disable]",     "Generate junk data stream on Serial3.");
+ShellCommand sc_logst("log",      "log start|stop|status",         "Start/Stop the logger, or check status.");
 
 class App : public Timer, public ShellHandler {
 private:
@@ -19,7 +20,9 @@ private:
   Shell m_one;
   Shell m_two;
 
-  bool bLogging;
+  bool bDemo;
+
+  LoggerSD m_SD;
 
   char log_filename_current[16];
   char log_filename_next[16];
@@ -29,7 +32,7 @@ public:
     m_list(this),
     m_one(Serial, m_list),
     m_two(Serial2, m_list),
-    bLogging(false)
+    bDemo(false)
   {
     pinMode(LED_BUILTIN, OUTPUT);
 
@@ -38,6 +41,8 @@ public:
     m_list.add(sc_lauto);
     m_list.add(sc_sdinf);
     m_list.add(sc_lsdir);
+    m_list.add(sc_demoj);
+    m_list.add(sc_logst);
     // ...
   }
   ~App() {
@@ -49,7 +54,9 @@ public:
   }
 
   virtual void every_10ms() { // runs once every 10ms, on average
-    //Serial3.println(analogRead(A0));
+    if (bDemo) {
+      Serial3.println(analogRead(A0));
+    }
   }
 
   virtual void every_tenth(int tenth) { // runs once every tenth of a second, where tenth = 0..9
@@ -64,140 +71,9 @@ public:
     m_one.tick();
     m_two.tick();
 
-    /*while (Serial && Serial1.available()) {
+    while (Serial && Serial1.available()) {
       Serial.write(Serial1.read());
-    }*/
-  }
-
-  bool cmd_sd_info(Shell& origin) {
-    char buf[64];
-
-    origin.write("SdFat version: ");
-    origin.write(SD_FAT_VERSION_STR);
-    origin.eol();
-
-    SdioConfig config(FIFO_SDIO);
-
-    uint32_t t = millis();
-    SdFs sd;
-    if (!sd.cardBegin(config)) {
-      origin.write("SD: initialization failed.\n");
-      if (isSpi(config)) {
-        origin.write("SD is a SPI device - check CS pin, etc.\n");
-      }
-      return false;
     }
-    t = millis() - t;
-    sprintf(buf, "SD: init time: %ldms\n", t);
-    origin.write(buf);
-
-    cid_t m_cid;
-    csd_t m_csd;
-    uint32_t m_ocr;
-
-    if (!sd.card()->readCID(&m_cid) || !sd.card()->readCSD(&m_csd) || !sd.card()->readOCR(&m_ocr)) {
-      origin.write("SD: readInfo failed\n");
-      return false;
-    }
-
-    origin.write("SD: card type: ");
-
-    switch (sd.card()->type()) {
-    case SD_CARD_TYPE_SD1:
-      origin.write("SD1\n");
-      break;
-
-    case SD_CARD_TYPE_SD2:
-      origin.write("SD2\n");
-      break;
-
-    case SD_CARD_TYPE_SDHC:
-      if (sdCardCapacity(&m_csd) < 70000000) {
-        origin.write("SDHC\n");
-      } else {
-        origin.write("SDXC\n");
-      }
-      break;
-
-    default:
-      origin.write("Unknown\n");
-    }
-
-    sprintf(buf, "SD: Manufacturer ID: %X; OEM ID: %c%c; Product: ", int(m_cid.mid), m_cid.oid[0], m_cid.oid[1]);
-    origin.write(buf);
-    for (uint8_t i = 0; i < 5; i++) {
-      buf[i] = m_cid.pnm[i];
-    }
-    buf[5] = 0;
-    origin.write(buf);
-    sprintf(buf, "; Version: %d.%d\n", int(m_cid.prv_n), int(m_cid.prv_m));
-    origin.write(buf);
-    sprintf(buf, "    Serial number: %lX", m_cid.psn);
-    origin.write(buf);
-    sprintf(buf, "; Manufacturing date: %d/%d", int(m_cid.mdt_month), (2000 + 16*m_cid.mdt_year_high + m_cid.mdt_year_low));
-    origin.write(buf);
-    sprintf(buf, "; OCR: %lX\n", m_ocr);
-    origin.write(buf);
-
-    MbrSector_t mbr;
-
-    if (!sd.card()->readSector(0, (uint8_t*) &mbr)) {
-      origin.write("SD: MBR read failed.\n");
-    }
-
-    bool valid = true;
-
-    origin.write("SD: Partition Table\n");
-
-    for (uint8_t ip = 1; ip < 5; ip++) {
-      MbrPart_t *pt = &mbr.part[ip - 1];
-      if ((pt->boot != 0 && pt->boot != 0x80) || getLe32(pt->relativeSectors) > sdCardCapacity(&m_csd)) {
-        valid = false;
-      }
-      char boot = (pt->boot == 0) ? '-' : ((pt->boot == 0x80) ? '*' : '?');
-      sprintf(buf, "  %c %d %02x %03d:%03d:%03d %03d:%03d:%03d %10ld %10ld\n", boot, ip, int(pt->type),
-              int(pt->beginCHS[0]), int(pt->beginCHS[1]), int(pt->beginCHS[2]), int(pt->endCHS[0]), int(pt->endCHS[1]), int(pt->endCHS[2]),
-              getLe32(pt->relativeSectors), getLe32(pt->totalSectors));
-      origin.write(buf);
-    }
-    if (!valid) {
-      origin.write("SD: MBR not valid, assuming Super Floppy format.\n");
-    }
-
-    return true;    
-  }
-
-  bool cmd_ls(Shell& origin, const char * path = "/") {
-    SdFs sd;
-    if (!sd.cardBegin(SdioConfig(FIFO_SDIO))) {
-      origin.write("SD: Error! No card?\n");
-      return false;
-    }
-    if (!sd.volumeBegin()) {
-      origin.write("SD: Error! Filesystem error\n");
-      return false;
-    }
-
-    if (!sd.exists(path)) {
-      origin.triple("SD: Error! '", path, "' not found\n");
-      return false;
-    }
-
-    FsFile file;
-    if (!file.open(path, O_RDONLY)) {
-      origin.triple("SD: Error! Unable to read '", path, "'\n");
-      return false;
-    }
-
-    if (file.isDir()) {
-      origin.write("SD: Folder\n");
-    } else {
-      origin.write("SD: File\n");
-    }
-
-    sd.ls(path, LS_DATE | LS_SIZE);
-
-    return true;
   }
 
   const char * log_name_next() {
@@ -207,6 +83,21 @@ public:
     sprintf(log_filename_next, "log-%u.txt", lo | (hi << 8));
 
     return log_filename_next;
+  }
+
+  void log_name_increment() {
+    unsigned int lo = EEPROM.read(0);
+    unsigned int hi = EEPROM.read(1);
+
+    if (++lo == 256) {
+      lo = 0;
+      if (++hi == 256) {
+        hi = 0;
+      }
+    }
+
+    EEPROM.write(0, lo);
+    EEPROM.write(1, hi);
   }
 
   bool auto_start() {
@@ -238,9 +129,35 @@ public:
         origin.write("autostart disabled\n");
       }
     } else if (args == "sd-info") {
-      cmd_sd_info(origin);
+      m_SD.cmd_sd_info(origin);
     } else if (args == "ls") {
-      cmd_ls(origin);
+      m_SD.cmd_ls(origin);
+    } else if (args == "demo") {
+      if (++args != "") {
+        if (args == "--enable")
+          bDemo = true;
+        else if (args == "--disable")
+          bDemo = false;
+      }
+      origin.triple("Junk datastream ", bDemo ? "en" : "dis", "abled on Serial3.\n");
+    } else if (args == "log") {
+      if (++args != "") {
+        if (args == "start") {
+          if (m_SD.logging()) {
+            origin.write("Logger is already active.\n");
+          } else {
+            strcpy(log_filename_current, log_name_next());
+            log_name_increment();
+          }
+        } else if (args == "stop")
+          m_SD.cmd_log_stop(&origin);
+        else if (args == "status") {
+          origin.triple("Logger: ", m_SD.logging() ? "Active" : "Inactive", "\n");
+        } else
+          ce = ce_IncorrectUsage;
+      } else {
+        ce = ce_IncorrectUsage;
+      }
     } else {
       origin.write("Oops! Command: \"");
       origin.write(args.c_str());
---
+++ examples/Logger/LoggerSD.hh
@@ -0,0 +1,272 @@
+/* Copyright 2022 Francis James Franklin
+ * 
+ * Adapted from examples in the https://github.com/greiman/SdFat library.
+ * 
+ * Open Source under the MIT License - see LICENSE in the project's root folder
+ */
+
+#ifndef CommaComms_LoggerSD_hh
+#define CommaComms_LoggerSD_hh
+
+#include <SdFat.h>
+#include <sdios.h>
+#include <RingBuf.h>
+
+#include "MultiShell.hh"
+
+#define LOG_FILE_SIZE     150000000
+#define RING_BUF_CAPACITY    204800
+
+class LoggerSD {
+private:
+  bool m_bLogging;
+
+  SdioConfig m_config;
+
+  SdFs m_sd;
+
+  cid_t m_cid;
+  csd_t m_csd;
+
+  uint32_t m_ocr;
+
+  MbrSector_t m_mbr;
+
+  FsFile m_logfile;
+  RingBuf<FsFile, RING_BUF_CAPACITY> m_rbuf;
+
+public:
+  LoggerSD() :
+    m_bLogging(false),
+    m_config(FIFO_SDIO)
+  {
+    // ...
+  }
+
+  ~LoggerSD() {
+    // ...
+  }
+
+  bool init(unsigned long *dt = 0, Shell *shell = 0) {
+    uint32_t t0 = millis();
+
+    if (!m_sd.cardBegin(m_config)) {
+      if (shell) {
+	shell->write("SD: initialization failed.\n");
+	if (isSpi(m_config)) {
+	  shell->write("SD is a SPI device - check CS pin, etc.\n");
+	}
+      }
+      return false;
+    }
+    if (dt)
+      *dt = millis() - t0;
+
+    if (!m_sd.card()->readCID(&m_cid) || !m_sd.card()->readCSD(&m_csd) || !m_sd.card()->readOCR(&m_ocr)) {
+      if (shell)
+        shell->write("SD: readInfo failed\n");
+      return false;
+    }
+
+    if (!m_sd.card()->readSector(0, (uint8_t*) &m_mbr)) {
+      if (shell)
+        shell->write("SD: MBR read failed.\n");
+      return false;
+    }
+
+    return true;
+  }
+  inline bool init(Shell& shell) {
+    return init(0, &shell);
+  }
+
+  bool cmd_sd_info(Shell& origin) {
+    char buf[64];
+
+    origin.triple("SdFat version: ", SD_FAT_VERSION_STR, "\n");
+
+    unsigned long dt;
+    if (!init(&dt, &origin))
+      return false;
+
+    sprintf(buf, "SD: init time: %ldms\n", dt);
+    origin.write(buf);
+
+    const char * card_type = "Unknown";
+
+    switch (m_sd.card()->type()) {
+    case SD_CARD_TYPE_SD1:
+      card_type = "SD1";
+      break;
+
+    case SD_CARD_TYPE_SD2:
+      card_type = "SD2";
+      break;
+
+    case SD_CARD_TYPE_SDHC:
+      if (sdCardCapacity(&m_csd) < 70000000) {
+        card_type = "SDHC";
+      } else {
+        card_type = "SDXC";
+      }
+      break;
+
+    default:
+      break;
+    }
+    origin.triple("SD: card type: ", card_type, "\n");
+
+    sprintf(buf, "SD: Manufacturer ID: %X; OEM ID: %c%c; Product: ", int(m_cid.mid), m_cid.oid[0], m_cid.oid[1]);
+    origin.write(buf);
+    for (uint8_t i = 0; i < 5; i++) {
+      buf[i] = m_cid.pnm[i];
+    }
+    buf[5] = 0;
+    origin.write(buf);
+    sprintf(buf, "; Version: %d.%d\n", int(m_cid.prv_n), int(m_cid.prv_m));
+    origin.write(buf);
+    sprintf(buf, "    Serial number: %lX", m_cid.psn);
+    origin.write(buf);
+    sprintf(buf, "; Manufacturing date: %d/%d", int(m_cid.mdt_month), (2000 + 16*m_cid.mdt_year_high + m_cid.mdt_year_low));
+    origin.write(buf);
+    sprintf(buf, "; OCR: %lX\n", m_ocr);
+    origin.write(buf);
+
+    bool valid = true;
+
+    origin.write("SD: Partition Table\n");
+
+    for (uint8_t ip = 1; ip < 5; ip++) {
+      MbrPart_t *pt = &m_mbr.part[ip - 1];
+      if ((pt->boot != 0 && pt->boot != 0x80) || getLe32(pt->relativeSectors) > sdCardCapacity(&m_csd)) {
+        valid = false;
+      }
+      char boot = (pt->boot == 0) ? '-' : ((pt->boot == 0x80) ? '*' : '?');
+      sprintf(buf, "  %c %d %02x %03d:%03d:%03d %03d:%03d:%03d %10ld %10ld\n", boot, ip, int(pt->type),
+              int(pt->beginCHS[0]), int(pt->beginCHS[1]), int(pt->beginCHS[2]), int(pt->endCHS[0]), int(pt->endCHS[1]), int(pt->endCHS[2]),
+              getLe32(pt->relativeSectors), getLe32(pt->totalSectors));
+      origin.write(buf);
+    }
+    if (!valid) {
+      origin.write("SD: MBR not valid, assuming Super Floppy format.\n");
+    }
+
+    return true;    
+  }
+
+  bool cmd_ls(Shell& origin, const char *path = "/") {
+    if (!init(origin))
+      return false;
+
+    if (!m_sd.volumeBegin()) {
+      origin.write("SD: Error! Filesystem error\n");
+      return false;
+    }
+
+    if (!m_sd.exists(path)) {
+      origin.triple("SD: Error! '", path, "' not found\n");
+      return false;
+    }
+
+    FsFile file;
+    if (!file.open(path, O_RDONLY)) {
+      origin.triple("SD: Error! Unable to read '", path, "'\n");
+      return false;
+    }
+
+    if (file.isDir()) {
+      origin.write("SD: Folder\n");
+    } else {
+      origin.write("SD: File\n");
+    }
+
+    m_sd.ls(path, LS_DATE | LS_SIZE);
+
+    return true;
+  }
+
+  inline bool logging() const { return m_bLogging; }
+
+  bool cmd_log_start(const char *path, Shell *shell = 0) {
+    if (m_bLogging) {
+      if (shell)
+        shell->write("Logging is already active.\n");
+      return false;
+    }
+
+    if (!init(0, shell))
+      return false;
+
+    if (!m_sd.volumeBegin()) {
+      if (shell)
+        shell->write("SD: Error! Filesystem error\n");
+      return false;
+    }
+
+    if (!m_logfile.open(path, O_RDWR | O_CREAT | O_TRUNC)) {
+      if (shell)
+	shell->triple("Error! Failed to open ", path, " for writing.\n");
+      return false;
+    }
+
+    if (!m_logfile.preAllocate(LOG_FILE_SIZE)) {
+      if (shell)
+	shell->write("Error! preAllocate failed\n");
+      m_logfile.close();
+      return false;
+    }
+
+    m_rbuf.begin(&m_logfile);
+
+    m_bLogging = true;
+    return true;
+  }
+
+  void cmd_log_stop(Shell *shell = 0) {
+    if (!m_bLogging) {
+      if (shell)
+        shell->write("Logger is not active.\n");
+      return;
+    }
+
+    m_rbuf.sync();
+    m_logfile.truncate();
+    m_logfile.close();
+
+    m_bLogging = false;
+  }
+
+  void cmd_log_write(const char *data, int length, Shell *shell = 0) {
+    if (!m_bLogging)
+      return;
+
+    size_t n = m_rbuf.bytesUsed();
+    if ((n + m_logfile.curPosition()) > (LOG_FILE_SIZE - 20)) {
+      if (shell)
+	shell->write("Logger: File full - quitting.\n");
+      cmd_log_stop();
+      return;
+    }
+
+    m_rbuf.write(data, length);
+
+    if (m_rbuf.getWriteError()) {
+      if (shell)
+        shell->write("Logger: write error (data too fast for logging)\n");
+    }
+  }
+
+  void cmd_log_tick() {
+    if (!m_bLogging)
+      return;
+
+    if (m_rbuf.bytesUsed() >= 512 && !m_logfile.isBusy()) {
+      if (512 != m_rbuf.writeOut(512)) {
+	cmd_log_stop();
+	return;
+      }
+    }
+  }
+};
+
+#endif /* !CommaComms_LoggerSD_hh */
